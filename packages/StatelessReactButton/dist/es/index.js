ec770ae696b9f3804ead988ac6df75c46a23e542
Ryan Zhu <rzhu@snaplogic.com>
2018-08-31 15:59:06 -0700

    build

---
+++ packages/StatelessReactButton/dist/es/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var StatelessReactButton = function StatelessReactButton(_ref) {
+  var handleOnclick = _ref.handleOnclick;
+
+  return React.createElement(
+    'button',
+    { onClick: handleOnclick },
+    'react stateless button'
+  );
+};
+
+export default StatelessReactButton;
